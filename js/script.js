e4dc7d53edc935572381ac810f087dc6c1569b66
javharabdusattorov <javharabdusattorov2008@gmail.com>
2022-04-28 15:14:34 +0500

    finish commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function() {
+    let elLink = $('.nav a');
+    $('.top').hide();
+    elLink.on('click', function(em) {
+        em.preventDefault();
+        elLink.removeClass('active');
+        let select = $(this).addClass('active').attr('href');
+        let target = $(select);
+        $('html, body').animate({scrollTop: target.offset().top - 150}, 1000)
+    });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 500) {
+            $('.top').fadeIn(500);
+            $('.bottom').fadeOut(500);
+        } else {
+            $('.top').fadeOut(500);
+            $('.bottom').fadeIn(500);
+        }
+
+        let scroll = $(window).scrollTop();
+
+        elLink.each(function() {
+            let selector = $(this).attr('href');
+            let target = $(selector);
+            if(scroll >= target.offset().top - 150) {
+                elLink.removeClass('active');
+                $(this).addClass('active'); 
+            }
+        });
+
+    });
+
+    $('.top').click(function() {
+        $('html, body').animate({scrollTop: 0}, 1000);
+    })
+    $('.bottom').click(function() {
+        $('html, body').animate({scrollTop: 2000}, 1000);
+    })
+})
